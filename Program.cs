bbf3088f88f3469c33e482cf2ab3743c77861c4c
Levente <gecsel@kkszki.hu>
2024-10-11 12:04:36 +0200

    Udvozles project is ready to use

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oraimunka
+{
+    internal class Program
+    {
+
+        public static void Udvozles()
+        {
+
+            int ora = DateTime.Now.Hour;
+            if (ora >= 5 && ora < 12)
+            {
+                Console.WriteLine("Jó reggelt!");
+            }
+            else if (ora >= 12 && ora < 18)
+            {
+                Console.WriteLine("Jó napot!");
+            }
+
+            else if (ora >= 18 && ora < 22)
+            {
+                Console.WriteLine("Jó estét!");
+            }
+           
+        }
+
+        public static void Bank()
+        {
+            
+
+        }
+
+        static void Main(string[] args)
+        {
+            Udvozles();
+            Console.WriteLine("");
+            Bank();
+        }
+    }
+}
