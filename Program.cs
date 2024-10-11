3685ca1db293411d9c8889e5e7043cdf42984871
Levente <gecsel@kkszki.hu>
2024-10-11 12:33:40 +0200

    Kamat project is ready to use

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,7 +32,18 @@ namespace oraimunka
 
         public static void Bank()
         {
-            
+            Console.Write("Adja meg, hogy mennyi összeget szeretne berakni a bankba: ");
+            int osszeg = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Adja meg mennyi évre szeretné berakni? ");
+            int ev = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Adja meg a kamatot százalékban: ");
+            double kamat = Convert.ToDouble(Console.ReadLine());
+
+
+            double vegso_osszeg = osszeg * Math.Pow(1 + kamat / 100, ev);
+
+            Console.WriteLine($"A lekötött összeg {ev} év múlva: {vegso_osszeg:F2} forint.");
+
 
         }
 
@@ -40,6 +52,7 @@ namespace oraimunka
             Udvozles();
             Console.WriteLine("");
             Bank();
+            Console.WriteLine("");
         }
     }
 }
